ebf2647fa21513bf3713b016431e46f4c00b66e3
Denner Evaldt Machado <dnrevaldt@gmail.com>
2015-02-20 11:18:08 -0200

    initial

---
+++ PDS/Controllers/siteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PDS.Controllers
+{
+    public class siteController : Controller
+    {
+        // GET: site
+        public ActionResult home()
+        {
+            return View();
+        }
+    }
+}
